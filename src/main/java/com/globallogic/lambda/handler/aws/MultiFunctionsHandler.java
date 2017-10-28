6be5f55db78398bdb1e4478326c48af56debcea7
sercasti <sercasti@gmail.com>
2017-10-28 13:53:30 -0300

    removed unnecessary files, added dependencies to run on localhost

--- src/main/java/com/globallogic/lambda/handler/aws/MultiFunctionsHandler.java
+++
@@ -1,6 +0,0 @@
-package com.globallogic.lambda.handler.aws;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootStreamHandler;
-
-public class MultiFunctionsHandler extends SpringBootStreamHandler {
-}
